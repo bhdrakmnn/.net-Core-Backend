835bfe78437be2e6181f84b5ee9b9de6cc6d8c88
Bahadır AKMAN <bahadirakman55@gmail.com>
2023-12-13 02:47:26 +0300

    Backend
    My backend work with .net core (6.0)

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity // select işlemi yapıcaksak bu read işlemi olacak.
+    {
+        IQueryable<T> GetAll(bool tracing = true); //sorgu üzerinde çalışmak istiyosak IQueryable kulllanırız.
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracing = true);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracing = true); //first or defoult' un asenkron fonksiyonunu kullanacağı için async keywordünü ekledik.
+        Task<T> GetByIdAsync(string id, bool tracing = true);
+    }
+}
---
+++ ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Order: BaseEntity
+    {
+        public Guid CustomerId { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public ICollection<Product> Products { get; set; } // Bir order' ın birden fazla product' ı olduğunu ifade ediyor.
+        public Customer Customers { get; set; } //Bir tane customer olabilir.
+    }
+}
